5bb263ca466178930c2e7f794bf4e7713c43b493
Torsher <torsher09@gmail.com>
2021-03-23 13:24:47 +0200

    added methods sum and sub

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        System.out.println(sum(a,b));
+        System.out.println(sub(a,b));
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
